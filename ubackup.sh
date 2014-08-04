92adc822bc2292b81983edc4f13df4351bb4a140
Manuel Gutierrez <manuel.gutierrez@etecsa.cu>
2014-08-04 10:22:12 -0400

    init

---
+++ ubackup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+backup_number=6
+base_name="backup.gz"
+backup_path="/opt/pgbackup/"
+
+cd $backup_path
+
+# clean any old temp backup
+rm -f *.tmp
+
+
+# change this line with your specific backup: pgdump, tar, rsync, etc..
+echo "$(date +%T)" > "$base_name".tmp
+
+
+# make sure the backups are getting done before discarding old ones
+if [ ! -f  "$base_name".tmp ]
+then
+    echo "ERROR: Backup file \"$base_name.tmp\" not found"
+    exit -1
+fi
+
+# delete the oldest
+rm -f $base_name.$backup_number
+
+# rotate backups
+for i in $(seq $backup_number -1 2)
+do
+    mv "$base_name".$(($i-1)) "$base_name".$i
+done
+
+# tmp is now the newest backup
+mv "$base_name".tmp "$base_name".1
